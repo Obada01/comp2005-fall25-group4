543acea28f3da9cce720bd7e9a489e9c97ec26bc
jess-ika3 <joileaboya@mun.ca>
2025-10-30 15:08:35 -0230

    Add files via upload

---
+++ Work in progress-iteration 2/Bridge.java
@@ -0,0 +1,47 @@
+import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
+import java.awt.Color;
+
+public class Bridge {
+    private ArrayList<Lilipad> connections;
+    private Color bridgeColor;
+    private int width;
+    private int height;
+    private boolean isAPermanentBridge;
+
+    public Bridge() {
+        this.connections= new ArrayList<>(2); // bridge can connect only two lilipads
+        this.bridgeColor= new Color(102,51,0);
+        this.width= 10;
+        this.height= 5;
+        this.isAPermanentBridge= false;
+    }
+
+    public void connectLiliPads(LiliPad lPad1, LiliPad lPad2) {
+        connections.add(lPad1);
+        connections.add(lPad2);
+    }
+
+    public Color getBridgeColor() {
+        return bridgeColor;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public boolean isAPermanentBridge() {
+        return isAPermanentBridge;
+    }
+
+    // setters for making bridges permanent. Specially for the bridges connecting to the home lilipads
+    public void setAsPermanentBridge(boolean permanentBridge) {
+        this.isAPermanentBridge= permanentBridge;
+    }
+
+}
